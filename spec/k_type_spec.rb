0e0a5453ff257ad697ff393ba12f176d9085e511
klueless-io <david@ideasmen.com.au>
2021-04-06 08:04:18 +1000

    first commit

---
+++ spec/k_type_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe KType do
+  it "has a version number" do
+    expect(KType::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
